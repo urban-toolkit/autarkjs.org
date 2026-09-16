44597f1ad30352d0f42e0d1a7a474c8815e4ccf5
Fabio Miranda <fabiom@gmail.com>
2026-09-16 17:11:00 -0400

    Show the autk-grammar npm version on the home page

--- guide/.vitepress/theme/components/HomeHeroLinks.vue
+++ guide/.vitepress/theme/components/HomeHeroLinks.vue
@@ -3,6 +3,9 @@
     Other ways to use Autark:
     <span class="hero-link-soon" title="Within a Python-based interactive notebook">pyautark (soon)</span>,
     <a href="/grammar/" title="Through a grammar">autk-grammar</a>
+    <a class="hero-link-badge" href="https://www.npmjs.com/package/@urban-toolkit/autk-grammar" title="autk-grammar on npm">
+      <img alt="autk-grammar npm version" src="https://img.shields.io/npm/v/@urban-toolkit/autk-grammar?color=f43f5e&logo=npm&labelColor=111827" />
+    </a>
   </p>
 </template>
 
@@ -28,6 +31,17 @@
 
 .hero-links a:hover {
   text-decoration: underline;
+}
+
+.hero-links .hero-link-badge {
+  display: inline-block;
+  margin-left: 6px;
+  vertical-align: middle;
+  line-height: 0;
+}
+
+.hero-link-badge img {
+  height: 20px;
 }
 
 .hero-link-soon {
